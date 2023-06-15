01eb9d57e7bc7f29ead9ad45a1019bce1d565d5e
Michael Perez <FreshOceans@gmail.com>
2023-06-15 10:21:34 -0400

    Update config.js

--- docs/scripts/config.js
+++ docs/scripts/config.js
@@ -1,13 +1,13 @@
 export default {
     // Token Implicit Grant Client ID
-    clientID: 'your-client-id-here',
+    clientID: '96201ac3-17a8-47cc-bdca-9f6fe3ce9d80',
 
     redirectUri: 'https://localhost/',
 
     genesysCloud: {
         // Genesys Cloud region
         // eg. 'mypurecloud.ie', 'euw2.pure.cloud', etc...
-        region: 'mypurecloud.com'
+        region: 'usw2.pure.cloud'
     },
 
     translateServiceURI: 'https://localhost/translate'
